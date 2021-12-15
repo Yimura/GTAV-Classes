c70bc27b45e5a798fbcfeaedd27af3f47800b609
Yimura <andreas.maerten@scarlet.be>
2021-12-15 16:07:30 +0100

    feat(Classes): Updated for 1.58

--- CPed.hpp
+++ CPed.hpp
@@ -11,14 +11,14 @@ class CPed
 {
 public:
 	char pad_0000[32]; //0x0000
-	class CPedModelInfo* m_ped_model_info; //0x0020
+	class CPedModelInfo *m_ped_model_info; //0x0020
 	uint8_t m_entity_type; //0x0028
 	char pad_0029[3]; //0x0029
 	uint8_t m_invisible; //0x002C
 	char pad_002D[1]; //0x002D
 	uint8_t m_freeze_momentum; //0x002E
 	char pad_002F[1]; //0x002F
-	class pgBase* m_posbase; //0x0030
+	class pgBase *m_posbase; //0x0030
 	char pad_0038[88]; //0x0038
 	rage::vector3 m_camera; //0x0090
 	char pad_009C[237]; //0x009C
@@ -32,20 +32,16 @@ public:
 	char pad_02A4[124]; //0x02A4
 	rage::vector3 m_velocity; //0x0320
 	char pad_032C[2564]; //0x032C
-	class CAutomobile* m_vehicle; //0x0D30
+	class CAutomobile *m_vehicle; //0x0D30
 	char pad_0D38[912]; //0x0D38
-	class CPlayerInfo* m_player_info; //0x10C8
+	class CPlayerInfo *m_player_info; //0x10C8
 	char pad_10D0[8]; //0x10D0
-	class CPedWeaponManager* m_weapon_manager; //0x10D8
-	char pad_10E0[812]; //0x10E0
-	uint8_t m_bike_seatbelt; //0x140C
-	char pad_140D[11]; //0x140D
-	uint8_t m_vehicle_seatbelt; //0x1418
-	char pad_1419[94]; //0x1419
-	uint8_t m_in_vehicle; //0x1477
-	char pad_1478[104]; //0x1478
-	float m_armor; //0x14E0
-}; //Size: 0x14E4
-static_assert(sizeof(CPed) == 0x14E4);
+	class CPedWeaponManager *m_weapon_manager; //0x10D8
+	char pad_10E0[907]; //0x10E0
+	uint8_t m_in_vehicle; //0x146B
+	char pad_146C[196]; //0x146C
+	float m_armor; //0x1530
+}; //Size: 0x1534
+static_assert(sizeof(CPed) == 0x1534);
 
 #pragma pack(pop)
