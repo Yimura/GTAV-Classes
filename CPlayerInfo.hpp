71d18b086dcd8de391ebb5668eec432c9ab0e4dc
Yimura <andreas.maerten@scarlet.be>
2021-12-17 23:21:20 +0100

    feat(CPlayerInfo): Swapped to inline classes

--- CPlayerInfo.hpp
+++ CPlayerInfo.hpp
@@ -2,44 +2,50 @@
 
 #pragma warning(push)
 #pragma warning(disable : 4201) // nonstandard extension used: nameless struct/union
-union netAddress
+class netAddress
 {
-	std::uint32_t m_raw;
-	struct
-	{
-		std::uint8_t m_field4;
-		std::uint8_t m_field3;
-		std::uint8_t m_field2;
-		std::uint8_t m_field1;
-	};
-};
-#pragma warning(pop)
+public:
+	uint8_t m_field4; //0x0000
+	uint8_t m_field3; //0x0001
+	uint8_t m_field2; //0x0002
+	uint8_t m_field1; //0x0003
+}; //Size: 0x0004
+static_assert(sizeof(netAddress) == 0x4);
 
-#pragma pack(push, 1)
+class netPlayerData
+{
+public:
+	char pad_0000[8]; //0x0000
+	uint64_t m_rockstar_id; //0x0008
+	char pad_0010[52]; //0x0010
+	class netAddress m_relay_ip; //0x0044
+	uint16_t m_relay_port; //0x0048
+	char pad_004A[2]; //0x004A
+	class netAddress m_external_ip; //0x004C
+	uint16_t m_external_port; //0x0050
+	char pad_0052[2]; //0x0052
+	class netAddress m_internal_ip; //0x0054
+	uint16_t m_internal_port; //0x0058
+	char pad_005A[6]; //0x005A
+	uint64_t m_host_token; //0x0060
+	char pad_0068[8]; //0x0068
+	uint64_t m_rockstar_id2; //0x0070
+	char pad_0078[12]; //0x0078
+	char m_name[20]; //0x0084
+}; //Size: 0x0098
+static_assert(sizeof(netPlayerData) == 0x98);
+
 class CPlayerInfo
 {
 public:
-	char pad_0000[40]; //0x0000
-	uint64_t m_rockstar_id; //0x0028
-	char pad_0030[52]; //0x0030
-	netAddress m_relay_ip; //0x0064
-	uint16_t m_relay_port; //0x0068
-	char pad_006A[2]; //0x006A
-	netAddress m_external_ip; //0x006C
-	uint16_t m_external_port; //0x0070
-	char pad_0072[2]; //0x0072
-	netAddress m_internal_ip; //0x0074
-	uint16_t m_internal_port; //0x0078
-	char pad_007A[22]; //0x007A
-	uint64_t m_rockstar_id2; //0x0090
-	char pad_0098[12]; //0x0098
-	char m_name[20]; //0x00A4
+	char pad_0000[32]; //0x0000
+	class netPlayerData m_net_player_data; //0x0020
 	char pad_00B8[184]; //0x00B8
 	float m_swim_speed; //0x0170
 	char pad_0174[20]; //0x0174
 	uint32_t m_water_proof; //0x0188
 	char pad_018C[92]; //0x018C
-	class CPed* m_ped; //0x01E8
+	class CPed *m_ped; //0x01E8
 	char pad_01F0[40]; //0x01F0
 	uint32_t m_frame_flags; //0x0218
 	char pad_021C[52]; //0x021C
