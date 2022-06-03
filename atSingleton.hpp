674370a1e672da819e2063bf08691baacf2a2ca9
Yimura <andreas.maerten@scarlet.be>
2022-06-03 14:27:35 +0200

    feat(CTunables): Added new class

---
+++ atSingleton.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace rage
+{
+    template <typename T>
+	struct atSingleton
+	{
+	private:
+		T* m_basePtr{};
+
+	public:
+		bool isValid() const
+		{
+			return m_basePtr != nullptr;
+		}
+
+		T* getInstance()
+		{
+			return m_basePtr;
+		}
+	};
+}
