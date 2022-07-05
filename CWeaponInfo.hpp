935747bfced5a91d70da4f070b52aca37e87f650
aa15032261 <aa15032261@gmail.com>
2022-07-05 21:16:42 +0800

    Added weapon impact type. (#10)
    Co-authored-by: Tom Chan <aa15032261@toms.directory>

--- CWeaponInfo.hpp
+++ CWeaponInfo.hpp
@@ -25,6 +25,96 @@ enum class eDamageType : int32_t
 	_0x1C8E59AE
 };
 
+enum class eImpactType : int32_t
+{
+	DEFAULT_BULLETS         = -1,
+	GRENADE                 = 0,
+	GRENADELAUNCHER         = 1,
+	STICKYBOMB              = 2,
+	MOLOTOV                 = 3,
+	ROCKET                  = 4,
+	TANKSHELL               = 5,
+	HI_OCTANE               = 6,
+	CAR                     = 7,
+	PLANE                   = 8,
+	PETROL_PUMP             = 9,
+	BIKE                    = 10,
+	DIR_STEAM               = 11,
+	DIR_FLAME               = 12,
+	DIR_WATER_HYDRANT       = 13,
+	DIR_GAS_CANISTER        = 14,
+	BOAT                    = 15,
+	SHIP_DESTROY            = 16,
+	TRUCK                   = 17,
+	MK2_EXPLOSIVE_BULLETS   = 18,
+	SMOKEGRENADELAUNCHER    = 19,
+	SMOKEGRENADE            = 20,
+	BZGAS                   = 21,
+	FLARE                   = 22,
+	GAS_CANISTER            = 23,
+	EXTINGUISHER_1          = 24,
+	PROGRAMMABLEAR          = 25,
+	TRAIN                   = 26,
+	BARREL                  = 27,
+	PROPANE                 = 28,
+	BLIMP                   = 29,
+	DIR_FLAME_EXPLODE       = 30,
+	TANKER                  = 31,
+	PLANE_ROCKET            = 32,
+	VEHICLE_BULLET          = 33,
+	GAS_TANK                = 34,
+	BIRD_CRAP               = 35,
+	RAILGUN                 = 36,
+	BLIMP2                  = 37,
+	FIREWORK                = 38,
+	SNOWBALL                = 39,
+	PROXMINE                = 40,
+	VALKYRIE_CANNON         = 41,
+	AIR_DEFENCE             = 42,
+	PIPEBOMB                = 43,
+	VEHICLEMINE             = 44,
+	EXPLOSIVEAMMO           = 45,
+	APCSHELL                = 46,
+	BOMB_CLUSTER            = 47,
+	BOMB_GAS                = 48,
+	BOMB_INCENDIARY         = 49,
+	BOMB_STANDARD           = 50,
+	TORPEDO                 = 51,
+	TORPEDO_UNDERWATER      = 52,
+	BOMBUSHKA_CANNON        = 53,
+	BOMB_CLUSTER_SECONDARY  = 54,
+	HUNTER_BARRAGE          = 55,
+	HUNTER_CANNON           = 56,
+	ROGUE_CANNON            = 57,
+	MINE_UNDERWATER         = 58,
+	ORBITAL_CANNON          = 59,
+	BOMB_STANDARD_WIDE      = 60,
+	EXPLOSIVEAMMO_SHOTGUN   = 61,
+	OPPRESSOR2_CANNON       = 62,
+	MORTAR_KINETIC          = 63,
+	VEHICLEMINE_KINETIC     = 64,
+	VEHICLEMINE_EMP         = 65,
+	VEHICLEMINE_SPIKE       = 66,
+	VEHICLEMINE_SLICK       = 67,
+	VEHICLEMINE_TAR         = 68,
+	SCRIPT_DRONE            = 69,
+	RAYGUN                  = 70,
+	BURIEDMINE              = 71,
+	SCRIPT_MISSILE          = 72,
+	RCTANK_ROCKET           = 73,
+	BOMB_WATER              = 74,
+	BOMB_WATER_SECONDARY    = 75,
+	EXTINGUISHER_2          = 76,
+	EXTINGUISHER_3          = 77,
+	EXTINGUISHER_4          = 78,
+	EXTINGUISHER_5          = 79,
+	EXTINGUISHER_6          = 80,
+	SCRIPT_MISSILE_LARGE    = 81,
+	SUBMARINE_BIG           = 82,
+	EMPLAUNCHER_EMP         = 83,
+	SPOOF_EXPLOSION         = 99
+};
+
 enum class eFireType : int32_t
 {
 	None,
@@ -52,7 +142,8 @@ class CWeaponInfo : public CItemInfo
 {
 public:
 	eDamageType m_damage_type; //0x0020
-	char pad_0024[48]; //0x0024
+	eImpactType m_impact_type; //0x0024
+	char pad_0028[44]; //0x0028
 	eFireType m_fire_type; //0x0054
 	eWheelSlot m_wheel_slot; //0x0058
 	uint32_t m_group; //0x005C
