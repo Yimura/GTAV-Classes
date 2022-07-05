82e9e41998f44a2f7e036c0d86b2e62395ac649b
Yimura <andreas.maerten@scarlet.be>
2022-07-05 15:14:12 +0200

    feat(CVehicleModelInfo): Added sub types, name, manufacturer and others

--- CVehicleModelInfo.hpp
+++ CVehicleModelInfo.hpp
@@ -1,11 +1,40 @@
 #pragma once
 
+#include "CBaseModelInfo.hpp"
+
 #include <cstdint>
 
-class CVehicleModelInfo
+enum class eVehicleType : std::uint32_t
+{
+    Car,
+    Plane,
+	Unk2,
+	Quadbike,
+	Unk4,
+	Unk5,
+	AmphibiousCar,
+	AmphibiousQuadbike,
+	Heli,
+	Unk9,
+	Unk10,
+	Bike,
+	Bicycle,
+	Boat,
+	Train
+};
+
+#pragma pack(push, 2)
+class CVehicleModelInfo : public CBaseModelInfo
 {
 public:
-	char pad_0000[1419]; //0x0000
-	uint8_t m_custom_type; //0x058B
-}; //Size: 0x058C
-static_assert(sizeof(CVehicleModelInfo) == 0x58C);
+	char pad_00A4[500]; //0x00A4
+	char m_name[12]; //0x0298
+	char m_manufacturer[12]; //0x02A4
+	char pad_02B0[144]; //0x02B0
+	eVehicleType m_vehicle_type; //0x0340
+	char pad_0344[572]; //0x0344
+	uint32_t m_is_jetski; //0x0580
+	char pad_0584[4]; //0x0584
+}; //Size: 0x0588
+static_assert(sizeof(CVehicleModelInfo) == 0x588);
+#pragma pack(pop)
