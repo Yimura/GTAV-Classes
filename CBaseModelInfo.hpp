71160fd6506f5c5561b6aa20be55d339f8cb7283
Yimura <andreas.maerten@scarlet.be>
2022-07-05 13:21:38 +0200

    feat(CBaseModelInfo): Added min/max dimensions

--- CBaseModelInfo.hpp
+++ CBaseModelInfo.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "vector.hpp"
+
 #include <cstdint>
 
 enum class eModelType : std::uint8_t
@@ -31,8 +33,12 @@ class CBaseModelInfo
 public:
 	char pad_0000[24]; //0x0000
 	uint32_t m_model_hash; //0x0018
-	char pad_001C[129]; //0x001C
-	eModelType m_model_type; //0x009D
+	char pad_001C[20]; //0x001C
+	rage::fvector3 m_min_dimensions; //0x0030
+	char pad_003C[4]; //0x003C
+	rage::fvector3 m_max_dimensions; //0x0040
+	char pad_004C[81]; //0x004C
+	uint8_t m_model_type; //0x009D
 	char pad_009E[6]; //0x009E
 }; //Size: 0x00A4
 static_assert(sizeof(CBaseModelInfo) == 0xA4);
