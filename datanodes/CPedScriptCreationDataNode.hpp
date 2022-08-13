5f10217263d7d807159abd14ae80c9d98f052c64
Reece Watson <45642596+SkiddyToast@users.noreply.github.com>
2022-08-13 04:40:01 -0400

    fix(DataNodes): Delete files that were moved.

--- datanodes/CPedScriptCreationDataNode.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class CPedScriptCreationDataNode
-{
-public:
-    char pad_0x0[192]; //0x0000
-    bool m_stay_in_car_when_jacked; //0x00C0
-}; //Size: 0x00C1
-static_assert(sizeof(CPedScriptCreationDataNode) == 0xC1);
