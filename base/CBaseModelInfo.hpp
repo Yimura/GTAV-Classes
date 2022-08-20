55192feff798162481dc298906a5fc34bf9ad85e
Reece Watson <45642596+SkiddyToast@users.noreply.github.com>
2022-08-20 12:38:43 -0400

    fix(CBaseModelInfo & CVehicleModelInfo): Restore model type enum and fix sizing.

--- base/CBaseModelInfo.hpp
+++ base/CBaseModelInfo.hpp
@@ -31,12 +31,12 @@ enum class eModelType : std::uint8_t
 class CBaseModelInfo
 {
 public:
-	char pad_0000[24]; //0x0000
-	uint32_t m_model_hash; //0x0018
-	char pad_001C[20]; //0x001C
-	rage::fvector4 m_axis_aligned_bounding_box_min; //0x0030
-	rage::fvector4 m_axis_aligned_bounding_box_max; //0x0040
-	uint32_t m_flags;
+    char pad_0000[24]; //0x0000
+    uint32_t m_model_hash; //0x0018
+    char pad_001C[20]; //0x001C
+    rage::fvector4 m_axis_aligned_bounding_box_min; //0x0030
+    rage::fvector4 m_axis_aligned_bounding_box_max; //0x0040
+    uint32_t m_flags;
     char pad_0054[4];
     uint16_t unk_0x58;
     char pad_005A[6];
@@ -51,9 +51,9 @@ public:
     char pad_0088[8];
     uint64_t unk_0x90;
     char pad_0098[5];
-    uint8_t m_model_type;
+    eModelType m_model_type;
     char pad_009E[6];
     uint64_t unk_0xA8;
-}; //Size: 0x00AC
-static_assert(sizeof(CBaseModelInfo) == 0xAC);
+}; //Size: 0x00B0
+static_assert(sizeof(CBaseModelInfo) == 0xB0);
 #pragma pack(pop)
