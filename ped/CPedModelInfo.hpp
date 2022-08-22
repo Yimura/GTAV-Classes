3ed4ad9620af308c7a9ef048eaf0fddc2dc42164
Yimura <andreas.maerten@scarlet.be>
2022-08-22 23:33:55 +0200

    fix(CPedModelInfo): Incorporate CBaseModelInfo

--- ped/CPedModelInfo.hpp
+++ ped/CPedModelInfo.hpp
@@ -1,11 +1,46 @@
 #pragma once
 
+#include "base/CBaseModelInfo.hpp"
+
 #include <cstdint>
 
-class CPedModelInfo
+class CPedModelInfo : CBaseModelInfo
 {
 public:
-	char pad_0000[24]; //0x0000
-	uint32_t m_model_hash; //0x0018
+	char gapB0[48];
+	uint64_t qwordE0;
+	uint32_t dwordE8;
+	char gapEC[4];
+	uint32_t dwordF0;
+	char gapF4[4];
+	uint64_t qwordF8;
+	uint32_t dword100;
+	uint64_t qword108;
+	uint64_t qword110;
+	uint64_t qword118;
+	uint64_t qword120;
+	uint64_t qword128;
+	uint32_t dword130;
+	char gap134[148];
+	uint64_t qword1C8;
+	uint32_t dword1D0;
+	uint64_t qword1D8;
+	uint32_t dword1E0;
+	char gap1E4[52];
+	uint64_t qword218;
+	char gap220[8];
+	uint64_t qword228;
+	uint32_t dword230;
+	char gap234[4];
+	uint32_t dword238;
+	char gap23C[12];
+	uint64_t qword248;
+	uint64_t qword250;
+	uint64_t qword258;
+	uint64_t qword260;
+	uint64_t qword268;
+	uint64_t qword270;
+	uint64_t qword278;
+	char gap280[16];
 }; //Size: 0x001C
-static_assert(sizeof(CPedModelInfo) == 0x1C);
+static_assert(sizeof(CPedModelInfo) == 0x290);
