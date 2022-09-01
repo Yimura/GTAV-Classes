cc195260c0637eeb5696f18c8525712cb184e1d0
Davide Beatrici <git@davidebeatrici.dev>
2022-09-01 20:45:14 +0200

    feat: Add CCameraAngles, CCameraManagerAngles, CGameCameraAngles, CPlayerAngles

---
+++ camera/CGameCameraAngles.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CCameraManagerAngles.hpp"
+
+class CGameCameraAngles
+{
+public:
+	CCameraManagerAngles* m_angles; //0x0000
+	char pad_0008[56]; //0x0008
+}; //Size: 0x0040
+static_assert(sizeof(CGameCameraAngles) == 0x40);
