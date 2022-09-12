74f635c21b4e68e3b9f0c5c25a540b3e3df090dc
altforfr <110702072+altforfr@users.noreply.github.com>
2022-09-12 16:31:45 +0200

    Update CPed.hpp

--- ped/CPed.hpp
+++ ped/CPed.hpp
@@ -8,6 +8,7 @@
 #include "CPedInventory.hpp"
 #include "../entities/fwEntity.hpp"
 #include "../rage/vector.hpp"
+#include "../CPedDrawHandler/CVehicleDrawHandler.hpp"
 
 #include <cstdint>
 
@@ -19,13 +20,11 @@ public:
 	CPedModelinfo* m_model_info; //0x0020 
 	char pad_0x0028[0x20]; //0x0028
 	CPedDrawHandler* m_draw_handler; //0x0048 
-	char pad_0x0050[0x230]; //0x0050
-	float m_health; //0x0280 
-	char pad_0x0284[0x1C]; //0x0284
-	float m_max_health; //0x02A0 
-	char pad_0x02A4[0x7C]; //0x02A4
+	char pad_0x0050[0x2D0]; //0x0050
 	rage::fvector3 m_velocity; //0x0320 
-	char pad_0x032C[0xA04]; //0x032C
+	char pad_0x032C[0x8A4]; //0x032C
+	CBuilding* m_building; //0x0BD0 
+	char pad_0x0BD8[0x158]; //0x0BD8
 	CVehicle* m_vehicle; //0x0D30 
 	char pad_0x0D38[0x390]; //0x0D38
 	CPlayerInfo* m_player_info; //0x10C8 
