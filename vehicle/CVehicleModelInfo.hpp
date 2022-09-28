7586981720b41734f2e9ac246205fb861f3710ae
Yimura <24669514+Yimura@users.noreply.github.com>
2022-09-29 00:45:26 +0200

    fix(CVehicleModelInfo): Correct eVehicleType enum

--- vehicle/CVehicleModelInfo.hpp
+++ vehicle/CVehicleModelInfo.hpp
@@ -6,23 +6,23 @@
 
 enum class eVehicleType : std::uint32_t
 {
-	Unknown = 4294967295,
-	Car = 0,
-	Plane = 1,
-	Trailer = 2,
-	Quadbike = 3,
-	Draft = 4, //Unused
-	SubmarineCar = 5,
-	AmphibiousAutomobile = 6,
-	AmphibiousQuadbike = 7,
-	Heli = 8,
-	Blimp = 9,
-	AutoGyro = 10, //Unused
-	Bike = 11,
-	Bicycle = 12,
-	Boat = 13,
-	Train = 14,
-	Submarine = 15
+	VEHICLE_TYPE_NONE = 4294967295,
+	VEHICLE_TYPE_CAR = 0,
+	VEHICLE_TYPE_PLANE = 1,
+	VEHICLE_TYPE_TRAILER = 2,
+	VEHICLE_TYPE_QUADBIKE = 3,
+	VEHICLE_TYPE_DRAFT = 4,
+	VEHICLE_TYPE_SUBMARINECAR = 5,
+	VEHICLE_TYPE_AMPHIBIOUS_AUTOMOBILE = 6,
+	VEHICLE_TYPE_AMPHIBIOUS_QUADBIKE = 7,
+	VEHICLE_TYPE_HELI = 8,
+	VEHICLE_TYPE_BLIMP = 9,
+	VEHICLE_TYPE_AUTOGYRO = 10,
+	VEHICLE_TYPE_BIKE = 11,
+	VEHICLE_TYPE_BICYCLE = 12,
+	VEHICLE_TYPE_BOAT = 13,
+	VEHICLE_TYPE_TRAIN = 14,
+	VEHICLE_TYPE_SUBMARINE = 15,
 };
 
 enum class eVehicleClass : std::uint8_t
