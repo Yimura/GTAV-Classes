d00b03ead4fb64433eab00d3301a19e41f4cf42d
Aure7138 <wyaure@163.com>
2022-10-30 21:05:56 +0800

    typo

--- base/phBoundCapsule.hpp
+++ base/phBoundCapsule.hpp
@@ -7,7 +7,7 @@ namespace rage
 	{
 	public:
 		char pad_0000[44]; //0x0000
-		float m_collsion; //0x002C
+		float m_collision; //0x002C
 	}; //Size: 0x0030
 	static_assert(sizeof(phBoundCapsule) == 0x30);
 
