826940996f770d7898aa163cdf57e596a1afa453
user <email@hostname>
2022-11-12 20:04:32 -0500

    Fix padding

--- network/CCommunications.hpp
+++ network/CCommunications.hpp
@@ -18,16 +18,16 @@ public:
 	char pad_1800[8]; //0x1800
 	class CVoiceConnection *m_connections[32]; //0x1808
 	uint32_t m_connection_count; //0x1908
-	char pad_190C[108]; //0x190C
+	char pad_190C[3508]; //0x190C
 }; //Size: 0x1978
-static_assert(sizeof(CVoice) == 0x1978);
+static_assert(sizeof(CVoice) == 0x26C0);
 
 class CCommunications
 {
 public:
 	char pad_0000[48]; //0x0000
 	class CVoice m_voice; //0x0030
-	char pad_26F0[280308]; //0x26F0
+	char pad_26F0[280300]; //0x26F0
 }; //Size: 0x46DE4
-static_assert(sizeof(CCommunications) == 0x46DE4);
+static_assert(sizeof(CCommunications) == 0x46DDC);
 #pragma pack(pop)
