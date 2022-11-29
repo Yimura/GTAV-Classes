3203d0b0ac2b028815779ab61970296a4c74d135
Yimura <24669514+Yimura@users.noreply.github.com>
2022-11-29 18:08:06 +0100

    feat(ChatData): Added other chat fields
    Co-authored-by: yubie <24496817+yubie-re@users.noreply.github.com>

--- network/ChatData.hpp
+++ network/ChatData.hpp
@@ -4,18 +4,22 @@
 class ChatData
 {
 public:
-    char pad_0000[4];
-    uint32_t m_unk04;
+    uint32_t m_timer_one; //0x0000
+    uint32_t m_timer_two; //0x0004
     uint32_t m_char_count; //0x0008
-    uint32_t m_seed; //0x000C
-    char pad_0010[4]; //0x0010
+    uint32_t m_key_held_time; //0x000C
+    uint32_t m_team_label; //0x0010
     uint8_t m_chat_open; //0x0014
-    char pad_0015[2]; //0x0015
+    uint8_t m_is_job; //0x0015
+    uint8_t m_disabled; //0x0016
     uint8_t m_not_typing; //0x0017
-    uint8_t m_unk18; //0x0018
-    char pad_0019[7]; //0x0019
-    uint32_t m_active_mode; //0x0020
+    uint32_t m_focus_mode; //0x0018
+    uint32_t m_chat_mode; //0x001C
+    uint32_t m_scaleform; //0x0020
     char pad_0024[8]; //0x0024
-    char16_t m_current_text[256]; //0x002C
-}; //Size: 0x022C
-static_assert(sizeof(ChatData) == 0x22C);
+    char16_t m_current_text[142]; //0x002C
+    uint32_t m_hud_color; //0x0148
+    uint8_t m_hud_color_override; // 0x014C
+    char pad_014D[43]; // 0x014D
+}; //Size: 0x0178
+static_assert(sizeof(ChatData) == 0x178);
