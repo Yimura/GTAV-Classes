d089d678f70058f3841b992c8522edd0719c87e9
user <email@hostname>
2022-12-13 22:06:27 -0500

    Fix indent

--- network/Network.hpp
+++ network/Network.hpp
@@ -18,164 +18,164 @@ static_assert(sizeof(MetricSessionMigrated) == 0x348);
 class NetworkGameConfig
 {
 public:
-    char pad_0000[48]; //0x0000
-    uint32_t m_public_slots; //0x0030
-    uint32_t m_private_slots; //0x0034
-    char pad_0038[272]; //0x0038
+	char pad_0000[48]; //0x0000
+	uint32_t m_public_slots; //0x0030
+	uint32_t m_private_slots; //0x0034
+	char pad_0038[272]; //0x0038
 }; //Size: 0x0148
 static_assert(sizeof(NetworkGameConfig) == 0x148);
 
 class NetworkGameFilterMatchmakingComponent
 {
 public:
-    // do not use for actual network filters, this will break things
-    inline void SetParameter(const char* name, int index, int value)
-    {
-        std::strcpy(m_param_names[index], name);
-        m_param_mappings[index] = index;
-        m_param_values[index] = value;
-        m_enabled_params_bitset |= (1 << index);
+	// do not use for actual network filters, this will break things
+	inline void SetParameter(const char* name, int index, int value)
+	{
+		std::strcpy(m_param_names[index], name);
+		m_param_mappings[index] = index;
+		m_param_values[index] = value;
+		m_enabled_params_bitset |= (1 << index);
 
-        if (m_num_parameters <= index)
-            m_num_parameters++;
-    }
+		if (m_num_parameters <= index)
+			m_num_parameters++;
+	}
 
-    uint32_t m_filter_type; //0x0000
-    char m_filter_name[24]; //0x0004
-    uint32_t m_num_parameters; //0x001C
-    uint16_t m_game_mode; //0x0020
-    uint16_t m_session_type; //0x0022
-    uint32_t m_param_unk[8]; //0x0024
-    char m_param_names[8][24]; //0x0044
-    char pad_0104[4]; //0x0104
-    uint32_t m_param_mappings[8]; //0x0108
-    char pad_0128[352]; //0x0128
-    uint32_t m_param_values[8]; //0x0288
-    char pad_02A8[96]; //0x02A8
-    uint32_t m_enabled_params_bitset; //0x0308
-    char pad_030C[8]; //0x030C
+	uint32_t m_filter_type; //0x0000
+	char m_filter_name[24]; //0x0004
+	uint32_t m_num_parameters; //0x001C
+	uint16_t m_game_mode; //0x0020
+	uint16_t m_session_type; //0x0022
+	uint32_t m_param_unk[8]; //0x0024
+	char m_param_names[8][24]; //0x0044
+	char pad_0104[4]; //0x0104
+	uint32_t m_param_mappings[8]; //0x0108
+	char pad_0128[352]; //0x0128
+	uint32_t m_param_values[8]; //0x0288
+	char pad_02A8[96]; //0x02A8
+	uint32_t m_enabled_params_bitset; //0x0308
+	char pad_030C[8]; //0x030C
 }; //Size: 0x0314
 static_assert(sizeof(NetworkGameFilterMatchmakingComponent) == 0x314);
 
 class MatchmakingAttributes
 {
 public:
-    uint32_t m_game_mode; //0x0000
-    uint32_t m_num_params; //0x0004
-    uint32_t m_param_unk[8]; //0x0008
-    char m_param_names[8][24]; //0x0028
-    uint32_t m_param_values[8]; //0x00E8
-    uint32_t m_params_bitset; //0x0108
+	uint32_t m_game_mode; //0x0000
+	uint32_t m_num_params; //0x0004
+	uint32_t m_param_unk[8]; //0x0008
+	char m_param_names[8][24]; //0x0028
+	uint32_t m_param_values[8]; //0x00E8
+	uint32_t m_params_bitset; //0x0108
 }; //Size: 0x010C
 static_assert(sizeof(MatchmakingAttributes) == 0x10C);
 
 class NetworkGameFilter
 {
 public:
-    virtual ~NetworkGameFilter() = default;
-    virtual void Reset() {};
+	virtual ~NetworkGameFilter() = default;
+	virtual void Reset() {};
 
-    uint32_t m_build_type; //0x0008
-    uint32_t m_discriminator; //0x000C
-    uint32_t m_discriminator_mapping; //0x0010
-    uint32_t m_region_mapping; //0x0014
-    uint32_t m_language_mapping; //0x0018
-    uint32_t m_mm_group_1_mapping; //0x001C
-    uint32_t m_mm_group_2_mapping; //0x0020
-    uint32_t m_activity_type_mapping; //0x0024
-    uint32_t m_activity_id_mapping; //0x0028
-    uint32_t m_activity_players_mapping; //0x002C
-    class NetworkGameFilterMatchmakingComponent m_matchmaking_component; //0x0030
+	uint32_t m_build_type; //0x0008
+	uint32_t m_discriminator; //0x000C
+	uint32_t m_discriminator_mapping; //0x0010
+	uint32_t m_region_mapping; //0x0014
+	uint32_t m_language_mapping; //0x0018
+	uint32_t m_mm_group_1_mapping; //0x001C
+	uint32_t m_mm_group_2_mapping; //0x0020
+	uint32_t m_activity_type_mapping; //0x0024
+	uint32_t m_activity_id_mapping; //0x0028
+	uint32_t m_activity_players_mapping; //0x002C
+	class NetworkGameFilterMatchmakingComponent m_matchmaking_component; //0x0030
 }; //Size: 0x0344
 static_assert(sizeof(NetworkGameFilter) == 0x344);
 
 class SessionInfoBackup
 {
 public:
-    class rage::rlSessionInfo m_session_info;
-    uint32_t m_unk; //0x0070
-    char pad_0074[4]; //0x0074
-    uint32_t m_flags; //0x0078
+	class rage::rlSessionInfo m_session_info;
+	uint32_t m_unk; //0x0070
+	char pad_0074[4]; //0x0074
+	uint32_t m_flags; //0x0078
 }; //Size: 0x007C
 static_assert(sizeof(SessionInfoBackup) == 0x7C);
 
 class MatchmakingSessionResult
 {
 public:
-    class rage::rlSessionDetail m_detail;
-    char pad_03B8[24]; //0x03B8
+	class rage::rlSessionDetail m_detail;
+	char pad_03B8[24]; //0x03B8
 }; //Size: 0x03D0
 static_assert(sizeof(MatchmakingSessionResult) == 0x3D0);
 
 class PlayerNameMapNode
 {
 public:
-    char m_name[24]; //0x0000
-    class rage::rlGamerHandle m_handle; //0x0018
-    class PlayerNameMapNode* m_next; //0x0028
-    class PlayerNameMapNode* m_prev; //0x0030
+	char m_name[24]; //0x0000
+	class rage::rlGamerHandle m_handle; //0x0018
+	class PlayerNameMapNode* m_next; //0x0028
+	class PlayerNameMapNode* m_prev; //0x0030
 }; //Size: 0x0038
 static_assert(sizeof(PlayerNameMapNode) == 0x38);
 
 class JoiningPlayerNameMap
 {
 public:
-    class PlayerNameMapNode m_names[100]; //0x0000
-    char pad_15E0[40]; //0x15E0
-    uint32_t m_num_name_nodes; //0x1608
-    char pad_160C[796]; //0x160C
+	class PlayerNameMapNode m_names[100]; //0x0000
+	char pad_15E0[40]; //0x15E0
+	uint32_t m_num_name_nodes; //0x1608
+	char pad_160C[796]; //0x160C
 }; //Size: 0x1928
 static_assert(sizeof(JoiningPlayerNameMap) == 0x1928);
 
 class CNetBlacklistNode
 {
 public:
-    class rage::rlGamerHandle m_handle; //0x0000
-    bool m_block_rejoin; //0x0010
-    char pad_0011[3]; //0x0011
-    uint32_t m_added_time; //0x0014
-    class CNetBlacklistNode* m_next; //0x0018
-    class CNetBlacklistNode* m_prev; //0x0020
+	class rage::rlGamerHandle m_handle; //0x0000
+	bool m_block_rejoin; //0x0010
+	char pad_0011[3]; //0x0011
+	uint32_t m_added_time; //0x0014
+	class CNetBlacklistNode* m_next; //0x0018
+	class CNetBlacklistNode* m_prev; //0x0020
 }; //Size: 0x0028
 static_assert(sizeof(CNetBlacklistNode) == 0x28);
 
 class CNetBlacklist
 {
 public:
-    class CNetBlacklistNode m_nodes[16]; //0x0000
-    class CNetBlacklistNode* m_head; //0x0280
-    class CNetBlacklistNode* m_tail; //0x0288
-    uint32_t m_free_nodes; //0x0290
-    char pad_0294[4]; //0x0294
-    class CNetBlacklistNode* m_start; //0x0298
-    char pad_02A0[24]; //0x02A0
+	class CNetBlacklistNode m_nodes[16]; //0x0000
+	class CNetBlacklistNode* m_head; //0x0280
+	class CNetBlacklistNode* m_tail; //0x0288
+	uint32_t m_free_nodes; //0x0290
+	char pad_0294[4]; //0x0294
+	class CNetBlacklistNode* m_start; //0x0298
+	char pad_02A0[24]; //0x02A0
 }; //Size: 0x02B8
 static_assert(sizeof(CNetBlacklist) == 0x2B8);
 
 class RemotePlayerData
 {
 public:
-    class rage::netGamePlayerData m_data[32]; //0x0000
-    uint32_t m_count; //0x0600
-    char pad_0604[4]; //0x0604
+	class rage::netGamePlayerData m_data[32]; //0x0000
+	uint32_t m_count; //0x0600
+	char pad_0604[4]; //0x0604
 }; //Size: 0x0608
 static_assert(sizeof(RemotePlayerData) == 0x608);
 
 class InvitedGamer
 {
 public:
-    class rage::rlGamerHandle m_handle;
-    char pad_0010[12]; //0x0010
-    uint32_t m_flags; //0x001C
+	class rage::rlGamerHandle m_handle;
+	char pad_0010[12]; //0x0010
+	uint32_t m_flags; //0x001C
 }; //Size: 0x0020
 static_assert(sizeof(InvitedGamer) == 0x20);
 
 class InvitedGamers
 {
 public:
-    class InvitedGamer m_invited_gamers[100]; //0x0000
-    uint32_t m_num_invited_gamers; //0x0C80
-    char pad_0C84[4]; //0x0C84
+	class InvitedGamer m_invited_gamers[100]; //0x0000
+	uint32_t m_num_invited_gamers; //0x0C80
+	char pad_0C84[4]; //0x0C84
 }; //Size: 0x0C88
 static_assert(sizeof(InvitedGamers) == 0xC88);
 
