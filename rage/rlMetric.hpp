579a8e5cf65b79e7cacede1e186af223e027e426
Bugisoft <58910128+BugisoftRSG@users.noreply.github.com>
2022-12-30 01:24:00 +0100

    feat(Metric): That happens if you have 2 thoughts at the same time

--- rage/rlMetric.hpp
+++ rage/rlMetric.hpp
@@ -12,7 +12,7 @@ namespace rage
 
         virtual int _0x10() { return 0; }; // returns a constant integer
 
-        virtual void _0x18() { return 0; };
+        virtual void _0x18();
 
         virtual const char* get_name() { return ""; };
 
