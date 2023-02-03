9981fd6bf48fe28370a49c969901659274daae1a
Yimura <24669514+Yimura@users.noreply.github.com>
2023-02-03 23:19:37 +0100

    fix(CPedIntelligence): incorrect classname

--- ped/CPedIntelligence.hpp
+++ ped/CPedIntelligence.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-class CPedOxygenInfo
+class CPedIntelligence
 {
 public:
     char pad_0000[632]; //0x0000
     float m_oxygen_time; //0x0278
 }; //Size: 0x027C
-static_assert(sizeof(CPedOxygenInfo) == 0x27C);
+static_assert(sizeof(CPedIntelligence) == 0x27C);
