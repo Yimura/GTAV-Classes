667b3c19ea563515b192345ae4d5e3f14656b2c7
Aure7138 <wyaure@163.com>
2023-02-19 06:38:52 +0800

    commit

--- rage/netSyncTree.hpp
+++ rage/netSyncTree.hpp
@@ -11,11 +11,16 @@ namespace rage
         virtual ~netSyncTree();
 
         char pad_0008[8]; //0x0008
-        netSyncNodeBase* m_sync_node; //0x0010
-        char pad_0018[8]; //0x0018
+        netSyncNodeBase* m_next_sync_node; //0x0010
+        netSyncNodeBase* m_last_sync_node; //0x0018
         uint32_t m_child_node_count; //0x0020
-        char pad_0024[12]; //0x0024
-    }; //Size: 0x0030
-    static_assert(sizeof(netSyncTree) == 0x30);
+        uint32_t m_unk_array_count; //0x0024
+        char pad_0028[8]; //0x0028
+        netSyncNodeBase* m_child_nodes[42]; //0x0030
+        uint32_t m_child_node_max_count; //0x0180
+        netSyncNodeBase* m_unk_array[32]; //0x0188
+        uint32_t m_unk_array_max_count; //0x0288
+    }; //Size: 0x0290
+    static_assert(sizeof(netSyncTree) == 0x290);
 #pragma pack(pop)
 }
