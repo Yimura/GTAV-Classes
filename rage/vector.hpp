fadbd2d441e0a1f7459ebae6b42afb976e373ad3
user <email@hostname>
2023-05-09 06:37:10 -0400

    feat(vector): add constructors

--- rage/vector.hpp
+++ rage/vector.hpp
@@ -2,44 +2,65 @@
 
 namespace rage
 {
-    template<typename T>
-    union vector2
-    {
-        T data[2];
-        struct { T x, y; };
-    };
-    
-    template<typename T>
-    union vector3
-    {
-        T data[3];
-        struct { T x, y, z; };
-    };
+	template<typename T>
+	union vector2
+	{
+		T data[2];
+		struct { T x, y; };
 
-    template<typename T>
-    union vector4
-    {
-        T data[4];
-        struct { T x, y, z, w; };
-    };
+		vector2(T x, T y) :
+			x(x),
+			y(y)
+		{
+		}
+	};
 
-    template<typename T>
-    union matrix34
-    {
-        T data[3][4];
-        struct { struct {T x, y, z, w; } rows[3];};
-    };
+	template<typename T>
+	union vector3
+	{
+		T data[3];
+		struct { T x, y, z; };
 
-    template<typename T>
-    union matrix44
-    {
-        T data[4][4];
-        struct { struct {T x, y, z, w; } rows[4];};
-    };
+		vector3(T x, T y, T z) :
+			x(x),
+			y(y),
+			z(z)
+		{
+		}
+	};
 
-    typedef vector2<float> fvector2;
-    typedef vector3<float> fvector3;
-    typedef vector4<float> fvector4;
-    typedef matrix34<float> fmatrix34;
-    typedef matrix44<float> fmatrix44;
+	template<typename T>
+	union vector4
+	{
+		T data[4];
+		struct { T x, y, z, w; };
+
+		vector4(T x, T y, T z, T w) :
+			x(x),
+			y(y),
+			z(z),
+			w(w)
+		{
+		}
+	};
+
+	template<typename T>
+	union matrix34
+	{
+		T data[3][4];
+		struct { struct { T x, y, z, w; } rows[3]; };
+	};
+
+	template<typename T>
+	union matrix44
+	{
+		T data[4][4];
+		struct { struct { T x, y, z, w; } rows[4]; };
+	};
+
+	typedef vector2<float> fvector2;
+	typedef vector3<float> fvector3;
+	typedef vector4<float> fvector4;
+	typedef matrix34<float> fmatrix34;
+	typedef matrix44<float> fmatrix44;
 }
