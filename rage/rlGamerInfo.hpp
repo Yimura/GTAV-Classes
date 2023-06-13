247e15682c15e89f2b9d419a7d508d9d1615a746
user <email@hostname>
2023-06-13 10:11:22 -0400

    Remove m_peer_id_2

--- rage/rlGamerInfo.hpp
+++ rage/rlGamerInfo.hpp
@@ -6,15 +6,14 @@
 namespace rage
 {
 #pragma pack(push,8)
-    class rlGamerInfo : public rlGamerInfoBase
-    {
-    public:
-        uint64_t m_host_token;
-        uint64_t m_peer_id_2;
-        rlGamerHandle m_gamer_handle_2;
-        uint32_t m_ros_privilege;
-        char m_name[17];
-    }; //Size: 0x0098
-    static_assert(sizeof(rlGamerInfo) == 0xF8);
+	class rlGamerInfo : public rlGamerInfoBase
+	{
+	public:
+		uint64_t m_host_token;
+		rlGamerHandle m_gamer_handle_2;
+		uint32_t m_ros_privilege;
+		char m_name[17];
+	}; //Size: 0x0098
+	static_assert(sizeof(rlGamerInfo) == 0xF0);
 #pragma pack(pop)
 }
