d8304d3e608dac2c22754962420c19f6e74b2c47
Quentin <xiaoxiao921@hotmail.fr>
2023-06-27 19:19:19 +0200

    script vector3: add to_string and << operator overload (#119)

--- script/scrVector.hpp
+++ script/scrVector.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "../rage/vector.hpp"
+#include <string>
+#include <sstream>
+#include <ostream>
 
 namespace rage
 {
@@ -62,6 +65,19 @@ namespace rage
 		bool operator!=(const scrVector& other)
 		{
 			return this->x != other.x || this->y != other.y || this->z != other.z;
+		}
+
+		std::string to_string() const
+		{
+			std::stringstream ss;
+			ss << *this;
+			return ss.str();
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const scrVector& vec)
+		{
+			os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+			return os;
 		}
 
 		alignas(8) float x{};
