7c7fa8c6b0c35d030083e334965214adb6a04cc7
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-06-30 20:45:17 +0200

    Create CPlayerExtendedGameStateNode.hpp

---
+++ netsync/nodes/player/CPlayerExtendedGameStateNode.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include "netsync/CProjectBaseSyncDataNode.hpp"
+
+#pragma pack(push, 1)
+class CPlayerExtendedGameStateNode : CSyncDataNodeInfrequent
+{
+public:
+    float waypoint_x;       //0x00C0
+    float waypoint_y;       //0x00C4
+    bool unk1;              //0x00C5
+    bool unk2;              //0x00C6
+    bool unk3;              //0x00C7
+    bool unk4;              //0x00C8
+    bool unk5;              //0x00C9
+    bool has_waypoint_data; //0x00CA
+    bool is_waypoint_set;   //0x00CB
+};
+constexpr auto waypoint_x_offset = offsetof(CPlayerExtendedGameStateNode, waypoint_x);
+static_assert(waypoint_x_offset == 0xC0);
+#pragma pack(pop)
