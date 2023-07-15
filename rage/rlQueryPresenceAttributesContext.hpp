f1ba296fb27893ba19005ba6397f0edfc5771674
maybegreat48 <96936658+maybegreat48@users.noreply.github.com>
2023-07-15 13:29:46 +0000

    Update rlQueryPresenceAttributesContext.hpp

--- rage/rlQueryPresenceAttributesContext.hpp
+++ rage/rlQueryPresenceAttributesContext.hpp
@@ -7,7 +7,11 @@ namespace rage
 	{
 	public:
 		char m_presence_attribute_key[64]; //0x0000
-		char m_presence_attribute_value[256]; //0x0040
+		union
+		{
+			char m_presence_attribute_string_value[256]; //0x0040
+			uint64_t m_presence_attribute_int_value;
+		};
 		uint32_t m_presence_attibute_type; //0x0140
 		char pad_0144[4]; //0x0144
 	}; //Size: 0x0148
