6949d5cb1a15af9594ba082f5203d03ad4df8154
maybegreat48 <96936658+maybegreat48@users.noreply.github.com>
2023-07-15 14:57:55 +0000

    Update rlScHandle.hpp

--- rage/rlScHandle.hpp
+++ rage/rlScHandle.hpp
@@ -4,21 +4,21 @@
 
 namespace rage
 {
-#pragma pack(push,8)
+#pragma pack(push, 8)
 	class rlScHandle
 	{
 	public:
 		uint64_t m_rockstar_id; //0x0000
-		char m_pad[2];
+		int16_t unk_0008;
 		uint8_t m_platform; //0x000A
-		uint8_t unk_0009; //0x000B
+		char pad[5]{};      //0x000B
 
 		inline rlScHandle() = default;
 
 		inline rlScHandle(uint64_t rockstar_id) :
-			m_rockstar_id(rockstar_id),
-			m_platform(3),
-			unk_0009(0)
+		    m_rockstar_id(rockstar_id),
+		    m_platform(3),
+		    unk_0008(0)
 		{
 		}
 	}; //Size: 0x0010
