ccb98ed59eba04bba94329821d98311a72cfdc3b
gir489 <100792176+gir489returns@users.noreply.github.com>
2023-07-19 16:30:19 -0400

    Added Blip classes. (#125)

---
+++ ui/CBlipEntry.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "blip_t.hpp"
+
+class CBlipEntry
+{
+public:
+	rage::Blip_t* m_pBlip; //0x0000 
+
+};//Size=0x0008
---
+++ ui/CBlipList.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CBlipEntry.hpp"
+
+class CBlipList
+{
+public:
+	CBlipEntry m_Blips[1500]; //0x0000
+
+};//Size=0x2F18
