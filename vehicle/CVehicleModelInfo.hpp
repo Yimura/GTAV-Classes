c04dd85fb8a266575d58735d2b9376c665a4ed14
thehorizon84 <hoztonchingchong@gmail.com>
2023-12-02 23:35:50 +0100

    veh abilities

--- vehicle/CVehicleModelInfo.hpp
+++ vehicle/CVehicleModelInfo.hpp
@@ -111,7 +111,9 @@ public:
     float m_min_seat_height; //0x0554
     char pad_0558[40]; //0x0558
     uint32_t m_is_jetski; //0x0580
-    char pad_0584[28]; //0x0584
-}; //Size: 0x05A0
-static_assert(sizeof(CVehicleModelInfo) == 0x5A0);
+    char pad_0584[7]; //0x0584
+    uint16_t m_ability_flag; // 0x058B
+    char pad_058D[26]; // 0x058D
+}; //Size: 0x05A7
+static_assert(sizeof(CVehicleModelInfo) == 0x5A7);
 #pragma pack(pop)
