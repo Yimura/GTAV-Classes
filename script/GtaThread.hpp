e8d8bdd2b3152253f6e1fde48720e4caeab19f83
maybegreat48 <96936658+maybegreat48@users.noreply.github.com>
2023-12-20 14:49:40 +0000

    Assorted fixes (#157)
    * feat(GtaThread): add force cleanup fields
    
    * fix(scrThreadContext): there is no TIMER_C
    
    * feat(GtaThread): add the flag
    
    * fix(scrProgram): add scrNativeHandler include
    
    * fix(scrNativeHandler): make data fields public
    
    ---------
    
    Co-authored-by: maybegreat48 <email@hostname>

--- script/GtaThread.hpp
+++ script/GtaThread.hpp
@@ -4,17 +4,23 @@
 class GtaThread : public rage::scrThread
 {
 public:
-	rage::joaat_t m_script_hash;                // 0x120
-	char m_padding3[0x14];                      // 0x124
-	std::int32_t m_instance_id;                 // 0x138
-	char m_padding4[0x04];                      // 0x13C
-	std::uint8_t m_flag1;                       // 0x140
-	bool m_safe_for_network_game;               // 0x141
-	char m_padding5[0x02];                      // 0x142
-	bool m_is_minigame_script;                  // 0x144
-	char m_padding6[0x02];                      // 0x145
-	bool m_can_be_paused;                       // 0x147
-	bool m_can_remove_blips_from_other_scripts; // 0x148
-	char m_padding7[0x0F];                      // 0x149
+	rage::joaat_t m_script_hash;                // 0x128
+	int m_force_cleanup_ip;                     // 0x12C
+	int m_force_cleanup_fp;                     // 0x130
+	int m_force_cleanup_sp;                     // 0x134
+	int m_force_cleanup_filter;                 // 0x138
+	int m_force_cleanup_cause;                  // 0x13C
+	std::int32_t m_instance_id;                 // 0x140
+	char m_padding4[0x04];                      // 0x144
+	std::uint8_t m_flag1;                       // 0x148
+	bool m_safe_for_network_game;               // 0x149
+	char m_padding5[0x02];                      // 0x14A
+	bool m_is_minigame_script;                  // 0x14C
+	char m_padding6[0x02];                      // 0x14D
+	bool m_can_be_paused;                       // 0x14F
+	bool m_can_remove_blips_from_other_scripts; // 0x150
+	char m_padding7[0x2];                       // 0x151
+	std::uint8_t m_force_cleanup_state;         // 0x153
+	char m_padding8[0xC];                       // 0x154
 };
 static_assert(sizeof(GtaThread) == 0x160);
