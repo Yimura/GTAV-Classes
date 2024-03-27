a3031a89788141f292a83d18587277e04d367c4a
gir489 <100792176+gir489returns@users.noreply.github.com>
2024-03-27 16:01:52 -0400

    Fixed alignment of GamerHandle to match the game's. (#163)
    Added some of the classes I ported from Phantom for the game's camera.

--- camera/CCameraAngles.hpp
+++ camera/CCameraAngles.hpp
@@ -4,9 +4,14 @@
 
 class CCameraAngles
 {
+private:
+	char pad_0000[704]; //0x0000
 public:
-    char pad_0000[960]; //0x0000
-    CPlayerAngles* angles; //0x03C0
-    char pad_03C8[60]; //0x03C8
-}; //Size: 0x0408
-static_assert(sizeof(CCameraAngles) == 0x408);
+	CPlayerAngles* m_vehicle; //0x02C0
+	CPlayerAngles* m_vehicle_two; //0x02C8
+private:
+	char pad_02D0[240]; //0x02D0
+public:
+	CPlayerAngles* m_player_first_person; //0x03C0
+}; //Size: 0x03C8
+static_assert(sizeof(CCameraAngles) == 0x3C8);
