f2f73a8004c7ab9275559292272be94411f3a115
Space V <40030799+ahcenezdh@users.noreply.github.com>
2024-04-30 23:36:45 +0200

    feat(netsync/nodes): update some data nodes (#164)

--- netsync/nodes/heli/CHeliHealthDataNode.hpp
+++ netsync/nodes/heli/CHeliHealthDataNode.hpp
@@ -12,14 +12,14 @@ public:
 	uint32_t m_rear_rotor_health;
 	bool m_can_tail_boom_break_off;
 	bool m_is_tail_boom_broken;
-	bool m_unk;
+	bool m_has_custom_health;
 	bool m_disable_explode_from_body_damage;
 	uint32_t m_body_health;
 	uint32_t m_petrol_tank_health;
 	uint32_t m_engine_health;
-	float m_unk_deformation_level;
-	float m_unk2_deformation_level;
-	float m_unk3_deformation_level;
+	float m_main_rotor_damage_scale;
+	float m_rear_rotor_damage_scale;
+	float m_tail_boom_damage_scale;
 };
 static_assert(sizeof(CHeliHealthDataNode) == 0x118);
 #pragma pack(pop)
