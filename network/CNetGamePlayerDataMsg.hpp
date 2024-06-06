aa973216cc5215ff9ff70f467b96637f417c9bdd
maybegreat48 <email@hostname>
2024-06-05 20:18:44 -0400

    return values

--- network/CNetGamePlayerDataMsg.hpp
+++ network/CNetGamePlayerDataMsg.hpp
@@ -10,10 +10,10 @@ namespace rage {
     {
     public:
         virtual ~playerDataMsg() = default;
-        virtual int GetBufferSize() {};
+        virtual int GetBufferSize() { return 0; };
         virtual void Log() {};
-        virtual bool Serialize(datBitBuffer* buffer) {};
-        virtual bool Deserialize(datBitBuffer* buffer) {};
+        virtual bool Serialize(datBitBuffer* buffer) { return true; };
+        virtual bool Deserialize(datBitBuffer* buffer) { return true; };
 
         uint32_t m_game_version; //0x0008
         uint32_t m_nat_type; //0x000C
