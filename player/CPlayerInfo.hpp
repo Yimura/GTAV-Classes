aaac0b2d026acea10d81ebcdb66708f3ffe18e56
gir489returns <gir489returns@outlook.com>
2024-07-04 12:36:54 -0400

    Updated CPlayerInfo for 1.69.

--- player/CPlayerInfo.hpp
+++ player/CPlayerInfo.hpp
@@ -34,28 +34,28 @@ public:
 	uint32_t m_frame_flags; //0x0270
 	char pad_0274[52]; //0x0274
 	uint32_t m_player_controls; //0x02A8
-	char pad_02AC[1248]; //0x02AC
-	float m_wanted_can_change; //0x078C
-	char pad_0790[304]; //0x0790
-	uint32_t m_npc_ignore; //0x08C0
-	char pad_08C4[12]; //0x08C4
-	bool m_is_wanted; //0x08D0
-	char pad_08D1[7]; //0x08D1
-	uint32_t m_wanted_level; //0x08D8
-	uint32_t m_wanted_level_display; //0x08DC
-	char pad_08E0[1120]; //0x08E0
-	float m_run_speed; //0x0D40
-	float m_stamina; //0x0D44
-	float m_stamina_regen; //0x0D48
-	char pad_0D4C[16]; //0x0D4C
-	float m_weapon_damage_mult; //0x0D5C
-	float m_weapon_defence_mult; //0x0D60
-	char pad_0D64[4]; //0x0D64
-	float m_melee_weapon_damage_mult; //0x0D68
-	float m_melee_damage_mult; //0x0D6C
-	float m_melee_defence_mult; //0x0D70
-	char pad_0D74[8]; //0x0D74
-	float m_melee_weapon_defence_mult; //0x0D7C
-}; //Size: 0x0D80
-static_assert(sizeof(CPlayerInfo) == 0xD80);
+	char pad_02AC[1264]; //0x02AC
+	float m_wanted_can_change; //0x079C
+	char pad_07A0[304]; //0x07A0
+	uint32_t m_npc_ignore; //0x08D0
+	char pad_08D4[12]; //0x08D4
+	bool m_is_wanted; //0x08E0
+	char pad_08E1[7]; //0x08E1
+	uint32_t m_wanted_level; //0x08E8
+	uint32_t m_wanted_level_display; //0x08EC
+	char pad_08F0[1120]; //0x08F0
+	float m_run_speed; //0x0D50
+	float m_stamina; //0x0D54
+	float m_stamina_regen; //0x0D58
+	char pad_0D5C[16]; //0x0D5C
+	float m_weapon_damage_mult; //0x0D6C
+	float m_weapon_defence_mult; //0x0D70
+	char pad_0D74[4]; //0x0D74
+	float m_melee_weapon_damage_mult; //0x0D78
+	float m_melee_damage_mult; //0x0D7C
+	float m_melee_defence_mult; //0x0D80
+	char pad_0D84[8]; //0x0D84
+	float m_melee_weapon_defence_mult; //0x0D8C
+}; //Size: 0x0D90
+static_assert(sizeof(CPlayerInfo) == 0xD90);
 #pragma pack(pop)
