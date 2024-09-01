8209a13b6f54d424671b231a1fa537aef202f323
Mr-X-GTA <110748953+Mr-X-GTA@users.noreply.github.com>
2024-09-01 15:17:31 +0200

    fix: use `char16_t` instead of `wchar_t `

--- misc/InputMethodEditor.hpp
+++ misc/InputMethodEditor.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <cstdint>
 
 #pragma pack(push, 8)
@@ -8,8 +7,8 @@ class InputMethodEditor
 public:
 	uint32_t m_count; //0x0000
 	uint32_t m_selected_index; //0x0004
-	wchar_t m_composition_string[31]; //0x0008
-	wchar_t m_candidate_list[9][31]; //0x0046
+	char16_t m_composition_string[31]; //0x0008
+	char16_t m_candidate_list[9][31]; //0x0046
 	bool m_active; //0x0274
 	char pad_0275[3]; //0x0275
 }; //Size: 0x0278
