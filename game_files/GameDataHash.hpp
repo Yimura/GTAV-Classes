e78c6b2788263cce800971d124b6d1358eeb5cbc
Mr-X-GTA <110748953+Mr-X-GTA@users.noreply.github.com>
2024-09-17 14:14:24 +0200

    feat(game_files): update GameDataHash

--- game_files/GameDataHash.hpp
+++ game_files/GameDataHash.hpp
@@ -7,7 +7,7 @@ class GameDataHash
 {
 public:
 	bool m_is_japanese_version;
-	std::array<rage::Obf32, 15> m_data;
+	std::array<rage::Obf32, 16> m_data;
 };
-static_assert(sizeof(GameDataHash) == 0xF4);
+static_assert(sizeof(GameDataHash) == 0x104);
 #pragma pack(pop)
